56fb6d58f170ee058d1c9b4b09730acf83b120e1
adamjijo <adamjijoisme@gmail.com>
2021-01-01 18:41:15 +0530

    Added argparse.
    Allows user to pass in arguments for the script.

--- indexdir.py
+++ indexdir.py
@@ -11,12 +11,19 @@ import argparse
 max_depth = float("inf")
 
 def recursive_file_search(paths, filename, store, depth):
+    """
+    Arguments:
+        Paths: Takes a list containing paths to directories that should be traversed
+        Filename: Filename to look for
+        Store: List to store filepaths into that have the required filename
+        Depth: Depth to which the file tree should be traversed
+    """
     if depth >= max_depth:
         return 0
     paths.sort()
     for path in paths:
         if not os.path.exists(path):
-            print(f"{path} does not exist.")
+            print("Path '" + path + "' does not exist.")
             continue
         path = os.path.realpath(path)
         if os.path.isdir(path):
@@ -33,8 +40,22 @@ def recursive_file_search(paths, filename, store, depth):
             # handle_file(path)
             store.append(os.path.join(path, file_))
 
-args = sys.argv[1:]
-file_to_be_searched = ""
-# max_depth = ""
+parser = argparse.ArgumentParser()
+parser.add_argument("depth",
+                    metavar="DEPTH",
+                    help="The depth up to which files should be indexed",
+                    type=int)
+parser.add_argument("filename",
+                    metavar="FILE",
+                    help="The filename to look for")
+parser.add_argument("directories",
+                    metavar="DIRS",
+                    help="The directory/directories to be indexed",
+                    nargs='+')
+args = parser.parse_args()
+print(args, args.depth, args.filename, args.directories)
+max_depth = args.depth
 results = []
-# recursive_file_search(args)
+recursive_file_search(args.directories, args.filename, results, 0)
+for result in results:
+    print(result)
